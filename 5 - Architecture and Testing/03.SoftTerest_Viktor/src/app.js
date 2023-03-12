2290976615ea13beb5944f871c334122bdfb623e
fanitaadamova <113979211+fanitaadamova@users.noreply.github.com>
2023-03-12 13:25:31 +0200

    update
    update

---
+++ 5 - Architecture and Testing/03.SoftTerest_Viktor/src/app.js
@@ -0,0 +1,33 @@
+import { logout } from "./api/users.js";
+import { initialize } from "./router.js";
+import { showCatalog } from "./views/catalog.js";
+import { showCreate } from "./views/create.js";
+import { showDetails } from "./views/details.js";
+import { showHome } from "./views/home.js";
+import { showLogin } from "./views/login.js";
+import { showRegister } from "./views/register.js";
+
+
+document.getElementById('views').remove();
+
+const links = {
+    '/': showHome,
+    '/catalog': showCatalog,
+    '/login': showLogin,
+    '/register': showRegister,
+    '/details': showDetails,
+    '/create': showCreate,
+    '/logout': onLogout
+};
+//router e return-натия context от router.js
+const router = initialize(links);
+router.updateNav();
+
+//Start application in Home view
+router.goTo('/');
+
+function onLogout() {
+    logout();
+    router.updateNav();
+    router.goTo('/');
+}
